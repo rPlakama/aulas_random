9af2ac36e364087109a4994a681790d0380f3e54
rPlakama <rPlakama@proton.me>
2025-08-29 16:42:02 -0300

    flake.nix iniciada.

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "JavaScript flake.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux"; 
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+
+	nodejs_latest
+	nodePackages.yarn
+	nodePackages.live-server
+
+        ];
+
+        shellHook = ''
+          echo "JS ENV Loaded."
+        '';
+      };
+    };
+}
+
+
