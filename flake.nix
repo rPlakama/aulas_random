85b55695c42c1be5c4c16f1fda9a8f9b443ad6d2
rPlakama <rPlakama@proton.me>
2025-09-04 21:14:55 -0300

    new file:   .gitignore 	new file:   flake.lock 	new file:   flake.nix 	new file:   index.html 	new file:   menu.css

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "JavaScript flake.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+
+          prettier
+          nodejs_latest
+          nodePackages.yarn
+          nodePackages.live-server
+          vscode-langservers-extracted
+
+        ];
+
+        shellHook = ''
+          echo "JS ENV Loaded."
+        '';
+      };
+    };
+}
